dac3dabb9e493db7157ec15e7ccee4c0a4cef8bd
Icemap <qizhi.wang@pingcap.com>
2023-03-08 18:06:13 +0800

    [Update] to serverless tier

--- tidb/init.go
+++ tidb/init.go
@@ -1,12 +1,12 @@
 package tidb
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	"github.com/pingcap-inc/ossinsight-plugin/config"
 	"sync"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
@@ -16,7 +16,16 @@ var (
 
 func createDB() {
 	tidbConfig := config.GetReadonlyConfig().Tidb
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4",
+
+	err := mysql.RegisterTLSConfig("tidb", &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		ServerName: tidbConfig.Host,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&tls=tidb",
 		tidbConfig.User, tidbConfig.Password, tidbConfig.Host, tidbConfig.Port, tidbConfig.Db)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
